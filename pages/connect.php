4d2bca0717f6da412a8a3715fe1503344cfcf0b8
17 Yen The <letuan11191@gmail.com>
2019-03-31 22:44:00 +0700

    ThayDoiLan1

--- pages/connect.php
+++ pages/connect.php
@@ -1,20 +1,39 @@
-<?php
-/* Database config */
-$db_host		= 'localhost';
-$db_user		= 'openworl_qlt';
-$db_pass		= 'owv20171@';
-$db_database	= 'openworl_qlt'; 
-
-/* End config */
-
-$db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$conn = mysqli_connect($db_host,$db_user,$db_pass,$db_database);
-	if($conn){
-		$setLang = mysqli_query($conn,"SET NAMES 'utf8'");
-	}else{
-		die("Kết nối thất bại ".mysqli_connect_error());
-	}
-
+<?php
+
+/* Database config */
+
+$db_host		= 'localhost';
+
+$db_user		= 'root';
+
+$db_pass		= '';
+
+$db_database	= 'openworl_qlt'; 
+
+
+
+/* End config */
+
+
+
+$db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+$conn = mysqli_connect($db_host,$db_user,$db_pass,$db_database);
+
+	if($conn){
+
+		$setLang = mysqli_query($conn,"SET NAMES 'utf8'");
+
+	}else{
+
+		die("Kết nối thất bại ".mysqli_connect_error());
+
+	}
+
+
+
 ?>
